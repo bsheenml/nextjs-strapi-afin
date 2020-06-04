c11d4b26decdd585111c1bac16b8229e53248d14
bsheenml <brendan.sheen@gmail.com>
2020-06-03 21:02:34 -0700

    Add getServerSideProps to get full list of resources for index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import Head from 'next/head'
+import fetch from 'isomorphic-unfetch'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="container">
       <h1> NextJS GraphCMS Playground</h1>
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/resoucres`)
+
+  const data = await res.json()
+
+  return {
+    props: {
+      resoucres: data
+    }
+  }
+}
+
+export default Home
